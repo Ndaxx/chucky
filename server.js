7a9aa3e8011622a22543013cccdbc4507d89be46
Ndaxx <34195709+Ndaxx@users.noreply.github.com>
2024-06-04 13:55:39 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-let input = document.getElementsByClassName("input")[0].value;
-let secondinp = document.getElementsByClassName("input1")[0].value;
+
 let button = document.getElementById("button");
 
 app.use(express.urlencoded({extended: true}));
+app.get("/", (req, res) => {
+    res.render("index")
+})
+app.get("/index", (req, res) => {
+
+    let input = document.getElementsByClassName("input")[0].value;
+    let secondinp = document.getElementsByClassName("input1")[0].value;
 
 
 const checkButton = (input === H || input === h) &&  (secondinp === U || secondinp === u);
@@ -18,5 +24,6 @@ const checkButton = (input === H || input === h) &&  (secondinp === U || secondi
     }else {
     console.log (err.message);
     res.render("index");
-
-};
+    };
+});
+app.listen(port , () => console.log('> Server is up and running on port : ' + port));
